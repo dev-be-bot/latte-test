877faf0c36a363ff910ae7a4836d6640c4f737e0
eclipse-theme <163603617+eclipse-themes@users.noreply.github.com>
2025-10-07 00:54:07 +0200

    versioning

--- bin/latte.js
+++ bin/latte.js
@@ -31,7 +31,7 @@ class LatteCLI {
   }
 
   async run() {
-    console.log('☕ Latte Test Framework v2.0.4\n');
+    console.log('☕ Latte Test Framework v2.0.5\n');
     console.log('Discovering and executing test files...\n');
 
     try {
